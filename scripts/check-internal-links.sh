5936121f8473a978cc52e6bd540f914544791474
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2021-12-29 04:51:14 -0500

    Parameterize link check in local scripts
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/check-internal-links.sh
+++ scripts/check-internal-links.sh
@@ -1,11 +1,10 @@
-npx hyperlink public/sitemap.xml --canonicalroot https://www.wildtechgarden.ca/ \
+npx hyperlink public/sitemap.xml --canonicalroot https://example.com/ \
     --internal \
     --skip "https://www.facebook.com/sharer.php" \
     --skip "https://linkedin.com/shareArticle" \
     --skip "https://twitter.com/share" \
     --skip "https://www.pexels.com" \
     --skip "https://pixabay.com" \
-    --skip "example.com" \
     --skip "example.net" \
     --todo "301 http" \
     --todo "fragment-redirect" \
